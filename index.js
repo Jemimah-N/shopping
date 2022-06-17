89dc4f6e327b4d486bd2f2fd25b2271b945e8dcb
Jemimah Naanyu <106058450+Jemimah-N@users.noreply.github.com>
2022-06-17 16:22:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,4 +12,51 @@ function nav(menu) {
         }
         return navBar
 }
-root.appendChild(nav(menu))
+function hero() {
+    let hero = document.createElement("h1")
+    hero.className = "hero"
+    let h1 = document.createElement("h1")
+    h1.innerHTML = "welcome to the store"
+    hero.appendChild(h1)
+    return hero;
+}
+
+function featuredproducts(products){
+    let featured = document.createElement("div")
+    featured.className = "featured"
+    let h2 = document.createElement("h2")
+    h2.innerHTML = "featured products"
+    featured.appendChild(h2);
+    
+    productscontainer.className = "grid";
+    products.forEach(function(product)) {
+        const productscontainer = document.createElement("div")
+        productscontainer.class = "product"
+        const productimage = document.createElement("img")
+        productsimage.src = product.image
+        productscontainer.appendChild(productimage)
+        const productname = document.createElement("h3")
+        productname.html = product.name
+        productscontainer.appendChild("productName")
+        const productPrice = document.createElement("p")
+        productPrice.innerhtml = product.price
+        productscontainer.appendChild(productPrice)
+        const productDescription = document.createElement("p")
+
+    }
+    return featured
+}
+function footer()
+let footer = document.createElement("footer")
+footer.className = "footer"
+let p = document.createElement("p")
+p.innerHTML = "shopping &copy;2022"
+footer.appendChild(p)
+return footer;
+{           
+root.appendChild(nav(menu));
+root.appendChild(hero());
+root.appendChild(featuredproducts());
+root.appendChild(footer())
+}
+
